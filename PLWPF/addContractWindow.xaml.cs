5e0ee15cd14bd12ae53d15312e7a6b2de7b8e67a
nachum shtauber <nachum800@gmail.com>
2018-01-07 13:02:07 +0200

    222

--- PLWPF/addContractWindow.xaml.cs
+++ PLWPF/addContractWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -23,27 +22,34 @@ namespace PLWPF
     /// </summary>
     public partial class AddContractWindow : Window
     {
-        private Mother contractMother=new Mother();
-        private IBL bl=BLSingleton.GetBL;
+        private static Mother ContractMother=new Mother();
+        IBL bl = BLSingleton.GetBL;
         public AddContractWindow(Mother mother)
         {
             InitializeComponent();
-            contractMother = mother;
+            ContractMother = mother;
             ChooseChildComboBox.ItemsSource = bl.GetChildrenByMother(mother.ID);
-            //BackgroundWorker worker = new BackgroundWorker();
-            //worker.RunWorkerAsync(BL_Tool.MatchingNannies(child.ID));
         }
 
-        private void ChooseChildComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void ChooseChildComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var child = (Child) ChooseChildComboBox.SelectedItem;
-            var nannyList=new List<Nanny>();
-            new Thread((ThreadStart)delegate { nannyList = BL_Tool.MatchingNannies(child.ID).ToList(); }).Start();
-           
-
-
+            var child =(Child) ChooseChildComboBox.SelectedItem;
+            List<Nanny> nannyList=new List<Nanny>();
+            new Thread((ThreadStart) delegate { nannyList = NannyList(child);}).Start();
+            
         }
 
-      
+        private List<Nanny> NannyList(Child child)
+        {
+            List<Nanny> nannyList;
+            nannyList = BL_Tool.MatchingNannies(child.ID).ToList();
+            this.Dispatcher.Invoke(new Action(() => { nannyDataGrid.ItemsSource = nannyList;
+                foreach (var nanny in nannyList)
+                {
+                    if(BL_Tool.MotherRequirements())
+                }
+            }));
+            return nannyList;
+        }
     }
 }
