e3fcddc90f0b06f2f46729161e5e8ac9a66ef0d3
nachum shtauber <nachum800@gmail.com>
2018-01-08 15:04:25 +0200

    id check

--- DAL/DAL_XML_imp.cs
+++ DAL/DAL_XML_imp.cs
@@ -5,20 +5,134 @@ using System.Text;
 using System.Threading.Tasks;
 using BE;
 using System.Xml.Linq;
+using DS;
+using static DS.DSxml;
 
 namespace DAL
 {
     internal class Dal_XML_imp : IDal
     {
+        public void AddNanny(Nanny nanny)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveNanny(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateNanny(Nanny nanny)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Nanny GetNanny(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public void AddMother(Mother mother)
         {
-           bool p=from x in 
-            DS.DSxml.Mothers.Add(m.toXML());
-            DS.DSxml.SaveMothers();
+            var temp = (from x in Mothers.Elements()
+                where Convert.ToInt32(x.Element("ID").Value) == mother.ID
+                select x).FirstOrDefault();
+            if(temp!=null)
+            {
+                Mothers.Add(mother.toXML());
+                SaveMothers();
+                
+            }
+            else throw new Exception("Mother with the same ID already exists...");
+                
+           
            
         }
+
+         public bool RemoveMother(int m)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateMother(Mother mother)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Mother GetMother(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddChild(Child child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveChild(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateChild(Child child)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Child GetChild(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+         public void AddContract(Contract c)
+        {
+            //ContractNannyChild contract = c.clone();
+            //contract.ContractId = MaxContractID() + 1;
+            //DS.DSxml.Contracts.Add(contract.toXML());
+
+            XElement contract = c.toXML();
+            contract.Element("ContractID").Value = (MaxContractID() + 1).ToString();
+            DS.DSxml.Contracts.Add(contract);
+            DS.DSxml.SaveContracts();
+
+        }
+
+        public bool RemoveContract(int contractNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateContract(Contract contract)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contract GetContract(int contractNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Nanny> GetNannies(Func<Nanny, bool> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Mother> GetMothers(Func<Mother, bool> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Child> GetChildrenByMother(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Contract> GetContracts(Func<Contract, bool> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
         //matanya
-        public 
+         
 
         private int MaxContractID()
         {
@@ -38,19 +152,7 @@ namespace DAL
             return result;
 
         }
-        public int addContract(Contract c)
-        {
-            //ContractNannyChild contract = c.clone();
-            //contract.ContractId = MaxContractID() + 1;
-            //DS.DSxml.Contracts.Add(contract.toXML());
-
-            XElement contract = c.toXML();
-            contract.Element("ContractID").Value = (MaxContractID() + 1).ToString();
-            DS.DSxml.Contracts.Add(contract);
-            DS.DSxml.SaveContracts();
-            return Int32.Parse(contract.Element("ContractID").Value);
-
-        }
+       
         public IEnumerable<Mother> getAllMothers()
         {
             XElement root = DS.DSxml.Mothers;
@@ -73,14 +175,26 @@ namespace DAL
             return result.AsEnumerable();
         }
 
-        public bool removeMother(Mother m)
+      
+
+        public bool RemoveContract(Contract c)
         {
             throw new NotImplementedException();
         }
 
-        public bool removeContract(Contract c)
+        public bool idCheck(int id)
         {
-            throw new NotImplementedException();
+           var check1=(from mother in Mothers.Elements()
+                       where Convert.ToInt32(mother.Element("ID").Value) == id
+               select mother).Any();
+            
+            var check2 = (from nanny in Nannies.Elements()
+                where Convert.ToInt32(nanny.Element("ID").Value) == id
+                select nanny).Any();
+            var check3 = (from child in Children.Elements()
+                where Convert.ToInt32(child.Element("ID").Value) == id
+                select child).Any();
+            return check3 && check2 && check1;
         }
     }
 }
